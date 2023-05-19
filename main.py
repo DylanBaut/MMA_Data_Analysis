38d2aef51ea22a8a7eaaafb1e94cf9970770229d
Dylan Bautista <dylanjudebautista@icloud.com>
2023-05-19 17:42:19 -0400

    gathered all UFC stats using multiprocessing

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
-from selenium import webdriver
 import requests
 from bs4 import BeautifulSoup
-import pandas as pd
 from multiprocessing import Pool
 import multiprocessing
 import time
-from multiprocessing import Pool, Manager 
+from multiprocessing import Pool 
 import csv
-
+from concurrent.futures import ThreadPoolExecutor
 
 def get_second_num(raw):
     new = raw.text.split()
@@ -17,55 +15,26 @@ def get_first_num(raw):
     new = raw.text.split()
     return new[0]
 
-def init_processes(a,b,c,d,e,f,g,h,i, j,k,l,m,n,o,p):
-    global Awins
-    Awins = a
-    global opponentA
-    opponentA = b
-    global method
-    method = c
-    global totalStrikesAttA
-    totalStrikesAttA = d
-    global totalStrikesLandA
-    totalStrikesLandA = e
-    global TDA
-    TDA = f
-    global CtrlTimeA
-    CtrlTimeA = g
-    global significantStrikesA
-    significantStrikesA = h
-    global KDA
-    KDA = i
+def get_decision(detail):
+    new = detail.text
+    new = new.replace('.','')
+    return [s for s in new.split() if s.isdigit()]
 
-    global opponentB
-    opponentB = j
-    global totalStrikesAttB
-    totalStrikesAttB = k
-    global totalStrikesLandB
-    totalStrikesLandB = l
-    global TDB
-    TDB = m
-    global CtrlTimeB
-    CtrlTimeB = n
-    global significantStrikesB
-    significantStrikesB = o
-    global KDB
-    KDB = p
 
 def get_links():
     links =[]
     base = "http://ufcstats.com"
-    website = f"{base}/statistics/events/completed"
+    website = f"{base}/statistics/events/completed?page=all"
     result3 = requests.get(website)
     content3 = result3.text
-    soup3 = BeautifulSoup(content3, "html.parser")
+    soup3 = BeautifulSoup(content3, "lxml")
     eventBox = soup3.find('table', class_="b-statistics__table-events")
 
     eventslinks = [link['href'] for link in eventBox.find_all('a', attrs={'class':'b-link b-link_style_black'} )]
     for event in eventslinks:
         result2 = requests.get(event)
         content2 = result2.text
-        soup2 = BeautifulSoup(content2, "html.parser")
+        soup2 = BeautifulSoup(content2, "lxml")
         box = soup2.find('tbody', class_="b-fight-details__table-body")
         rows = box.find_all('tr', attrs={'class':'b-fight-details__table-row b-fight-details__table-row__hover js-fight-details-click'} )
         for row in rows:
@@ -74,98 +43,136 @@ def get_links():
 
 
 def get_response(url):
+    dataList =[]
+
     result = requests.get(url)
+    if(result.status_code != 200):
+        print(result.status_code, "Status_code")
+        time.sleep(0.5)
+        return get_response(url)
     content = result.text
-    soup = BeautifulSoup(content, "html.parser")
+    soup = BeautifulSoup(content, "lxml")
     
     totalStrikesAt = soup.select_one(".js-fight-section .b-fight-details__table-col:nth-child(5) .b-fight-details__table-text:nth-child(1)")
-    TDAt = soup.select_one(".js-fight-section .b-fight-details__table-col:nth-child(6) .b-fight-details__table-text:nth-child(1)")
-    CtrlTimeAt = soup.select_one(".b-fight-details__table-col:nth-child(10) .b-fight-details__table-text:nth-child(1)")
-    significantStrikesAt = soup.select_one(".js-fight-section .b-fight-details__table-col:nth-child(3) .b-fight-details__table-text:nth-child(1)")
-    KDAt = soup.select_one(".js-fight-section .l-page_align_left+ .b-fight-details__table-col .b-fight-details__table-text:nth-child(1)")
     opponentAt = soup.select_one(".js-fight-section .b-fight-details__table-text:nth-child(1) .b-link_style_black")
-    
+    KDA = soup.select('.js-fight-table .l-page_align_left+ .b-fight-details__table-col .b-fight-details__table-text:nth-child(1)')
+    TDA = soup.select('.js-fight-table .b-fight-details__table-col:nth-child(6) .b-fight-details__table-text:nth-child(1)')
+    SubA =soup.select('.js-fight-table .b-fight-details__table-col:nth-child(8) .b-fight-details__table-text:nth-child(1)')
+    CtrlA = soup.select('.js-fight-table .b-fight-details__table-col:nth-child(10) .b-fight-details__table-text:nth-child(1)')
+    HeadA = soup.select('table+ .js-fight-section .b-fight-details__table-col:nth-child(4) .b-fight-details__table-text:nth-child(1)')
+
     totalStrikesBt = soup.select_one(".js-fight-section .b-fight-details__table-col:nth-child(5) .b-fight-details__table-text+ .b-fight-details__table-text")
-    TDBt = soup.select_one(".js-fight-section .b-fight-details__table-col:nth-child(6) .b-fight-details__table-text+ .b-fight-details__table-text")
-    CtrlTimeBt = soup.select_one(".b-fight-details__table-col:nth-child(10) .b-fight-details__table-text+ .b-fight-details__table-text")
-    significantStrikesBt = soup.select_one(".js-fight-section .b-fight-details__table-col:nth-child(3) .b-fight-details__table-text+ .b-fight-details__table-text")
-    KDBt = soup.select_one(".js-fight-section .l-page_align_left+ .b-fight-details__table-col .b-fight-details__table-text+ .b-fight-details__table-text")
     opponentBt = soup.select_one(".js-fight-section .b-fight-details__table-text+ .b-fight-details__table-text .b-link_style_black")
+    KDB = soup.select('.js-fight-table .l-page_align_left+ .b-fight-details__table-col .b-fight-details__table-text+ .b-fight-details__table-text')
+    TDB = soup.select('.js-fight-table .b-fight-details__table-col:nth-child(6) .b-fight-details__table-text+ .b-fight-details__table-text')
+    SubB =soup.select('.js-fight-table .b-fight-details__table-col:nth-child(8) .b-fight-details__table-text+ .b-fight-details__table-text')
+    CtrlB = soup.select('.js-fight-table .b-fight-details__table-col:nth-child(10) .b-fight-details__table-text+ .b-fight-details__table-text')
+    HeadB =soup.select('table+ .js-fight-section .b-fight-details__table-col:nth-child(4) .b-fight-details__table-text+ .b-fight-details__table-text')
+
     methodt = soup.select_one(".b-fight-details__label+ i")
-
     opponentAdiv = soup.select_one(".b-fight-details__person:nth-child(1)")
-    winLoss = opponentAdiv.find('i')
-    if(winLoss.text.strip()=="W"):
-        Awins.append(1)
-    else:
-        Awins.append(0)
-    
-    totalStrikesAttA.append(get_second_num(totalStrikesAt))
-    totalStrikesLandA.append(get_first_num(totalStrikesAt))
-    TDA.append(get_first_num(TDAt))
-    CtrlTimeA.append(CtrlTimeAt.text.strip())
-    significantStrikesA.append(get_first_num(significantStrikesAt))
-    KDA.append(KDAt.text.strip())
-    opponentA.append(opponentAt.text.strip())
-    
-    totalStrikesAttB.append(get_second_num(totalStrikesBt))
-    totalStrikesLandB.append(get_first_num(totalStrikesBt))
-    TDB.append(get_first_num(TDBt))
-    CtrlTimeB.append(CtrlTimeBt.text.strip())
-    significantStrikesB.append(get_first_num(significantStrikesBt))
-    KDB.append(KDBt.text.strip())
-    opponentB.append(opponentBt.text.strip())
+    rounds = soup.select_one(".b-fight-details__text-item:nth-child(4)")
+    Arounds = soup.select(".b-fight-details__bar-chart-text_style_light-red")
+    Brounds = soup.select(".b-fight-details__bar-chart-text_style_light-blue")
+    roundEnd = soup.select_one(".b-fight-details__text-item_first+ .b-fight-details__text-item")
+    endRound =int(roundEnd.text.split()[1])
+    numrounds = rounds.text.split()[2]
 
+    if(numrounds=='No'):
+        dataList.append("Error_skip")
+        return dataList
+    
+    if(((int(numrounds)!=5) & (int(numrounds)!= 3))| (totalStrikesAt is None)):
+        dataList.append("Error_skip")
+        return dataList
+    
+    dataList.append(numrounds)
+
+    winLoss = opponentAdiv.find('i')            
+    if(winLoss.text.strip()=="W"):
+        dataList.append(1)
+    else:
+        dataList.append(0)
+
+    decision = False
     methodWords = methodt.text.split()
     if(methodWords[0]== "KO/TKO"):
-        method.append(0)
+        dataList.append(0)
     elif(methodWords[0]== "Submission"):
-        method.append(1)
+        dataList.append(1)
     elif(methodWords[0]=="Decision"):
         if(methodWords[2]=="Unanimous"):
-            method.append(2)
+            dataList.append(2)
         else:
-            method.append(3)#split
+            dataList.append(3)#split
+        decision =True
     else:
-        method.append(4)#DQ/NC
+        dataList.append(4)#DQ/NC/Draw
+
+    dataList.append(opponentAt.text.strip())
+    dataList.append(get_second_num(totalStrikesAt))
+    for i in range(endRound):
+        dataList.append(get_second_num(Arounds[i]))
+        dataList.append(get_first_num(Arounds[i]))
+        dataList.append(KDA[i].text.strip())
+        dataList.append(get_first_num(TDA[i]))
+        dataList.append(SubA[i].text.strip())
+        dataList.append(CtrlA[i].text.strip())
+        dataList.append(get_first_num(HeadA[i]))
+
+    if(endRound != 5):
+        for i in range((5-endRound)*7):
+            dataList.append('--')
+
+    
+    dataList.append(opponentBt.text.strip())
+    dataList.append(get_second_num(totalStrikesBt))
+    for i in range(endRound):
+        dataList.append(get_second_num(Brounds[i]))
+        dataList.append(get_first_num(Brounds[i]))
+        dataList.append(KDB[i].text.strip())
+        dataList.append(get_first_num(TDB[i]))
+        dataList.append(SubB[i].text.strip())
+        dataList.append(CtrlB[i].text.strip())
+        dataList.append(get_first_num(HeadB[i]))
+    if(endRound != 5):
+        for i in range((5-endRound)*7):
+            dataList.append('--')
+    if(decision):
+        details = soup.select(".b-fight-details__text+ .b-fight-details__text .b-fight-details__text-item")
+        if(details is not None):
+            for detail in details:
+                dataList.append(get_decision(detail)[0] +' '+ get_decision(detail)[1])
+    
+    return dataList
     
 
 def main():
-    print
     start_time = time.time()
-    linkList = get_links()
     cores = multiprocessing.cpu_count()
-    manager = Manager()
-
-    Awins = manager.list()
-    opponentA = manager.list()
-    method = manager.list()
-    totalStrikesAttA = manager.list()
-    totalStrikesLandA = manager.list()
-    TDA = manager.list()
-    CtrlTimeA = manager.list()
-    significantStrikesA = manager.list()     
-    KDA =manager.list()
-
-    opponentB = manager.list()
-    totalStrikesAttB = manager.list()
-    totalStrikesLandB = manager.list()
-    TDB = manager.list()
-    CtrlTimeB = manager.list()
-    significantStrikesB = manager.list()     
-    KDB =manager.list()
-
-    with Pool(int(cores/2), initializer=init_processes, initargs=(Awins,opponentA,method,totalStrikesAttA,totalStrikesLandA,TDA,CtrlTimeA,significantStrikesA,KDA, opponentB,totalStrikesAttB,totalStrikesLandB,TDB,CtrlTimeB,significantStrikesB,KDB)) as p:
-        p.map(get_response, linkList)
-
-    rows = zip(Awins, method, opponentA, totalStrikesAttA, totalStrikesLandA,TDA,CtrlTimeA,significantStrikesA,KDA, opponentB, totalStrikesAttB, totalStrikesLandB,TDB,CtrlTimeB,significantStrikesB,KDB)
+    linkList = get_links()
+    with Pool(int(cores/2)) as p:
+        rows= p.map(get_response, linkList)
     with open('UFC.csv', "w") as f:
         writer = csv.writer(f)
-        writer.writerow(['Opponent A Wins','method','Opponent A','Total Strikes Attempted (A)','Total Strikes Landed (A)', 'Takedowns (A)', 
-                         'Control Time (A)', 'Significant Strikes: (A)','Knockdowns (A)','Opponent B','Total Strikes Attempted (B)','Total Strikes Landed (B)', 'Takedowns (B)', 
-                         'Control Time (B)', 'Significant Strikes: (B)','Knockdowns (B)'])
+        writer.writerow(['Number of Rounds','Opponent A Wins','Method',
+                         'Opponent A','Total Strikes Attempted (A)',
+                         'Round 1 Sig Strikes Attempted (A)','Round 1 Sig Strikes Landed (A)','Round 1 KD (A)','Round 1 TD (A)', 'Round 1 Sub Attempts (A)','Round 1 Ctrl Time (A)', 'Round 1 Head Strikes (A)',
+                         'Round 2 Sig Strikes Attempted (A)','Round 2 Sig Strikes Landed (A)','Round 2 KD (A)','Round 2 TD (A)', 'Round 2 Sub Attempts (A)','Round 2 Ctrl Time (A)', 'Round 2 Head Strikes (A)',
+                         'Round 3 Sig Strikes Attempted (A)','Round 3 Sig Strikes Landed (A)','Round 3 KD (A)','Round 3 TD (A)', 'Round 3 Sub Attempts (A)','Round 3 Ctrl Time (A)', 'Round 3 Head Strikes (A)',
+                         'Round 4 Sig Strikes Attempted (A)','Round 4 Sig Strikes Landed (A)','Round 4 KD (A)','Round 4 TD (A)', 'Round 4 Sub Attempts (A)','Round 4 Ctrl Time (A)', 'Round 4 Head Strikes (A)',
+                         'Round 5 Sig Strikes Attempted (A)','Round 5 Sig Strikes Landed (A)','Round 5 KD (A)','Round 5 TD (A)', 'Round 5 Sub Attempts (A)','Round 5 Ctrl Time (A)', 'Round 5 Head Strikes (A)',
+                         'Opponent B','Total Strikes Attempted (B)',
+                         'Round 1 Sig Strikes Attempted (B)','Round 1 Sig Strikes Landed (B)','Round 1 KD (B)','Round 1 TD (B)', 'Round 1 Sub Attempts (B)','Round 1 Ctrl Time (B)', 'Round 1 Head Strikes (B)',
+                         'Round 2 Sig Strikes Attempted (B)','Round 2 Sig Strikes Landed (B)','Round 2 KD (B)','Round 2 TD (B)', 'Round 2 Sub Attempts (B)','Round 2 Ctrl Time (B)', 'Round 2 Head Strikes (B)',
+                         'Round 3 Sig Strikes Attempted (B)','Round 3 Sig Strikes Landed (B)','Round 3 KD (B)','Round 3 TD (B)', 'Round 3 Sub Attempts (B)','Round 3 Ctrl Time (B)', 'Round 3 Head Strikes (B)',
+                         'Round 4 Sig Strikes Attempted (B)','Round 4 Sig Strikes Landed (B)','Round 4 KD (B)','Round 4 TD (B)', 'Round 4 Sub Attempts (B)','Round 4 Ctrl Time (B)', 'Round 4 Head Strikes (B)',
+                         'Round 5 Sig Strikes Attempted (B)','Round 5 Sig Strikes Landed (B)','Round 5 KD (B)','Round 5 TD (B)', 'Round 5 Sub Attempts (B)','Round 5 Ctrl Time (B)', 'Round 5 Head Strikes (B)',
+                         'Decision1','Decision2','Decision3', 
+                         ])
         for row in rows:
-            writer.writerow(row)
+            if(row[0]!='Error_skip'):
+                writer.writerow(row)
     print(f"{(time.time() - start_time):.2f} seconds")
     
 if __name__ == '__main__':
